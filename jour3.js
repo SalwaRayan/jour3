fac8ce049f9bc9ffea242cbff92d8cf21f4fdb82
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-09-28 20:39:17 +0200

    jour3

---
+++ jour3.js
@@ -0,0 +1,109 @@
+// 01 - Correction
+
+var fruits = ["mango", "lemon", "blueberry"]
+
+console.log(fruits)
+// console.table(fruits)
+
+// 02 - Access
+
+var ingredients = ["eggs", "milk", "butter"]
+
+console.log(ingredients[1])
+console.log(ingredients[2])
+
+// 03 - Add and Remove
+
+var objects = ["pen", "book", "lamp"]
+
+console.log(objects)
+
+objects.unshift("chair")
+console.log(objects)
+
+objects.pop()
+console.log(objects)
+
+objects.push("laptop")
+console.log(objects)
+
+objects.shift()
+console.log(objects)
+
+// 04 - Order
+
+var numbers = [4, 10, 8, 12, 6]
+
+numbers.reverse()
+console.log(numbers)
+
+numbers.sort((a, b) => a - b) 
+console.log(numbers)
+
+// 05 - Boucle
+
+var total = 0
+var limit = 10
+
+for (var i = 1; i <= limit ; total = total + i++){
+  console.log(total + i)
+}
+
+// 06 - Reverse
+
+var sentence = "Hello Konexio !"
+
+var reverseSentence = "";
+
+for (var i = sentence.length - 1; i >= 0; i--) {
+  reverseSentence += sentence[i];
+}
+console.log(reverseSentence)
+
+// Bonus
+
+for(var i = 0;  i <= 100; i++)
+
+if (i % 3 === 0 && i % 5 === 0) {
+  console.log(i, "fizzbuzz")
+} else if (i % 5 === 0) {
+  console.log(i, "buzz")
+} else if (i % 3 === 0) {
+  console.log(i, "fizz")
+} else if (i % 7 === 0) {
+  console.log("")
+} else {
+  console.log(i)
+}
+
+// Bonus II
+
+var total = 0
+var limit = 10
+var i = 1
+
+while (i <= limit){
+  total = total + i++
+}
+console.log(total)
+
+//  Bonus III
+
+var mate = ["Ahmed", "KévinA", "Alin", "Bilal", "Chaïma", "Edouard", "Eloi", "Evan", "Hanaa", "Hélène", "Jeremy", "Karimou", "KévinJ", "Marco", "Rébecca", "Syrine", "Jong", "Vincent"]
+var randomMate = mate[Math.floor(Math.random()*mate.length)]
+
+console.log(randomMate)
+
+// Bonus IV
+
+var array = []
+var number = ""
+
+
+  for (; array.length <= 20 ; number = Math.floor(Math.random()*(100 - 0 + 1) + 0)) {
+    array.push(number)
+  }
+
+  console.table(array)
+
+  for ()
